c8d6af3020762e9656a223000d494cc5d87add1e
unknown <andrei.baetic@gmail.com>
2018-07-12 23:15:09 -0400

    added html writer, fully parsed files, added points to dicts

--- scoreboardManager.py
+++ scoreboardManager.py
@@ -2,6 +2,28 @@ import sys
 import os
 import re
 from operator import itemgetter
+import json
+
+def notInCombined(dict, lst):
+    for i in lst:
+        if i['hash'] == dict['hash']:
+            return False
+    return True
+    
+def dictCombiner(lst):
+    combinedList = []
+    for sublist in lst:
+        for i in sublist:
+            if notInCombined(i, combinedList):
+                combinedList.append(i)
+            else:
+                for j in combinedList:
+                    if j['hash'] == i['hash']:
+                        j['points'] += i['points']
+                        j['gamesPlayed'] += i['gamesPlayed']
+    combinedList = sorted(combinedList, key=itemgetter('points'), reverse=True)        
+    return combinedList
+
 
 def dirToDict(dir):
 
@@ -14,7 +36,6 @@ def dirToDict(dir):
             for line in file:
                 line = re.sub(' +',' ', line)
                 data = line.split()
-                print(line)
                 dictList.append({'hash' : data[0], 'chips' : data[1], 'gamesPlayed' : data[2]})
             #sorting, assigning score
             dictList = sorted(dictList, key=itemgetter('chips'), reverse=True)
@@ -23,15 +44,25 @@ def dirToDict(dir):
                 if score >= 20:
                     score -= 20
                 dict['points'] = score + 10
+                dict.pop('chips', None)
             finalList.append(dictList)
-            
+        
+    return finalList
 
 if __name__ == '__main__':
 
+    os.mkdir('./data')
     dir = sys.argv[1]
 
-    dirToDict(dir)
+    scoreList = dictCombiner(dirToDict(dir))
+    #scoreList = json.dumps(test)
 
-
+    with open('scoreboard.html', 'w') as file:
+        file.write('<table > <table border="1"> <tbody>')
+        file.write('<tr> <td> Team </td> <td> Hash </td> <td> Score </td> </tr>')
+        for team in scoreList:
+            file.write('<tr> <td> muie dragnea </td> <td>' + team['hash'] + '</td> <td>' + str(team['points']) + '</td> </tr>')
+        file.write('</tbody> </table>')
+            
 
 
